ff0b95240e559b4b2b3f49967569a6eaed357b5b
qizhangumich <qizhan@umich.edu>
2021-11-15 21:52:52 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 mkdir -p ~/.streamlit/
+
 echo "\
 [server]\n\
 headless = true\n\
-const PORT = process.env.PORT || 3000;\n\
+port = $PORT\n\
 enableCORS = false\n\
 \n\
 " > ~/.streamlit/config.toml
